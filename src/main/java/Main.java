c63a8c32df15f1cb831a59d7fca6c73f79dd88d9
Varga György <vargagyurivip@gmail.com>
2021-09-08 19:42:19 +0200

    create database communication, one bug fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import aplication.DBEngine;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        DBEngine engine = new DBEngine();
+
+        System.out.println(engine.isConnected());
     }
 }
--- src/main/java/aplication/DBEngine.java
+++ src/main/java/aplication/DBEngine.java
@@ -1,4 +1,34 @@
 package aplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBEngine {
+
+    private Connection connection;
+
+    public DBEngine(){
+        connection = connect();
+    }
+    public boolean isConnected() {
+        return (connection != null);
+    }
+
+    private Connection connect() {
+        String url = "jdbc:mysql://localhost:3306/workClothesDB" + "?" +
+                "useUnicode=yes" + "&" +
+                "characterEncoding=UTF-8";
+
+        Properties properties = new Properties();
+        properties.put("user", "root");
+        properties.put("password", "Riddick84");
+
+        try {
+            return DriverManager.getConnection(url, properties);
+        } catch (SQLException e) {
+            return null;
+        }
+    }
 }
