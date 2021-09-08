8397e676e9da67f9bd1d173f0708a053e1ba85a6
Varga György <vargagyurivip@gmail.com>
2021-09-08 19:03:01 +0200

    Added Position Enem and added fields to the Employs class

--- src/main/java/models/Employs.java
+++ src/main/java/models/Employs.java
@@ -1,4 +1,16 @@
 package models;
 
 public class Employs {
+
+    private String name;
+    private Enum Position;
+    private String t_ShirtSyze;
+    private String sweaterSyze;
+    private String vestSyze;
+    private String jacketSyze;
+    private String trousersSyze;
+    private String shortsSyze;
+    private int safetyBootsSyze;
+
+
 }
---
+++ src/main/java/models/Position.java
@@ -0,0 +1,10 @@
+package models;
+
+public enum Position {
+
+    OFFICE_WORKER,
+    MECHANIC,
+    ELECTRICIAN,
+    MAINTENANCE,
+    INDOOR_WORKER
+}
